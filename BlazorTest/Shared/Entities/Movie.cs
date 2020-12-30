a09c9cf79c16c6f983e8474435fede4200dcc16c
fylstathis3 <fylstathis@gmail.com>
2020-12-30 20:14:53 +0200

    Initial setup

---
+++ BlazorTest/Shared/Entities/Movie.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace BlazorTest.Shared.Entities
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
